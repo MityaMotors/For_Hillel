b2111cc8d469a96682cf1438fee82519a3cfbc71
Dima <mityamotors232@gmail.com>
2021-12-13 15:35:59 +0200

    Added the homework_4

---
+++ homework_4/homework_4.py
@@ -0,0 +1,10 @@
+print("Hello teacher!!! This is my Escape Sequence table. Hope I figured out the issue;) ")
+print("""The Name   | The Value
+\\a         | Bell (alert)
+\\b         | Backspace
+\\n         | New line
+\\t         | Horizontal tab
+\\\\         | Backslash \\
+\\"         | Double quotation mark "
+\\'         | Single quotation mark '
+""")
