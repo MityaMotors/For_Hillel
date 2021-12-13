395d9614fd08bd2eeef9b6dc1bb53481f4d235e3
Dima <mityamotors232@gmail.com>
2021-12-13 18:20:35 +0200

    last commit

--- homework_5/homework_5.py
+++ homework_5/homework_5.py
@@ -4,14 +4,14 @@ Var=" ".join(s1)
 print(s1)
 s2="Names"
 for line in [s2]:
-    print ("{:+^90}".format(s2))
+    print("{:+^90}".format(s2))
 for line in [s1]:
-    print ("{:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90}" .format(*line))
+    print("{:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90} \n {:>90}" .format(*line))
 
 St = "name=Amanda=sssss&age=32&&salary=1500&currency=quro"
 St_my="name&Amanda=sssss"
-St_my1=St_my.split("&")
-St1=St.split("&")
+St_1=St.split("&")
+St1=St_1.split("&")
 St2=St1[0]
 St3=St1[1]
 St4=St2.split("&")
@@ -22,11 +22,11 @@ St8=St5[1]
 St8=St8.split("=")
 St9=St5[1]
 St9=St9.split("=")
-V2=St_my1+St6+St8+St9
+V2=St1+St6+St8+St9
 keys=[]
 values=[]
 for i, j in enumerate(V2):
-    if i%2==0:
+    if i% 2==0:
         keys.append(j)
     else:
         values.append(j)
