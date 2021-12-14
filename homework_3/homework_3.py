cfa9b7e578bb9b7e4b3a6d95dc4654333f945cf7
Dima <mityamotors232@gmail.com>
2021-12-14 11:58:43 +0200

    a bit changes

--- homework_3/homework_3.py
+++ homework_3/homework_3.py
@@ -1,8 +1,8 @@
 print("Application launching ...")
-user = input("Enter username")
+user = input("Enter username: ")
 #set breakpoint here to get "user" variable value. Just print it in Console
 print(f'Hello {user}! Please choose a number:')
-number = input("0-100")
+number = input("0-100: ")
 """Add "pass" instruction on the last line of the script
 to get "number" variable value in debug mode.
 Set breakpoint on line with pass."""
