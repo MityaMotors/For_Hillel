c8dba25eff139a91a5a345a6f3686768c2bb620b
Dima <mityamotors232@gmail.com>
2021-12-15 11:57:09 +0200

    Add homework_9

--- homework_6/homework_6.py
+++ homework_6/homework_6.py
@@ -1,5 +1,20 @@
-print("Hello")
+user = "Tim"
+data1 = ["John", "Maria", "Antom", "Tim"]
+data2 = ["John", "Maria", "Antom", "tIm"]
+data = ["John", "Maria", "antom", "tim"]
 
-class Chocolate:
-    def __init__(self,):
+if user not in data1:
+    data1.append(user)
 
+if user not in data2:
+    data2.append(user)
+elif user.lower() in data2:
+    data.append(user)
+
+found = False
+for item in data:
+    if item.lower() == user.lower():
+        found = True
+        break
+if not found:
+        data.append(user)
